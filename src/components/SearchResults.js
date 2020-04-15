c971235fdd86d11cbf9bae8a6406e2a330aecbf5
kumackey <kumak1t09e0@gmail.com>
2020-04-15 16:59:36 +0900

    create cards of search results

---
+++ src/components/SearchResults.js
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+
+import AppContext from "../contexts/AppContext";
+
+import SearchResult from "./SearchResult";
+
+const SearchResults = () => {
+  const { state } = useContext(AppContext);
+
+  return (
+    <>
+      <h4>検索結果</h4>
+      {state.searchResults.map((searchResult, index) => {
+        return <SearchResult key={index} searchResult={searchResult} />;
+      })}
+    </>
+  );
+};
+
+export default SearchResults;
