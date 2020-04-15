aa01c77ce22153a0bcf9345852deffc37bd89b5d
kumackey <kumak1t09e0@gmail.com>
2020-04-15 16:52:33 +0900

    fix typo

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,11 @@ import React, { useReducer } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import AppContext from "../contexts/AppContext";
+
 import reducer from "../reducers";
 
 import SearchBooksForm from "./SearchBooksForm";
+import SearchResults from "./SearchResults";
 
 const App = () => {
   const initialState = {
@@ -18,6 +20,7 @@ const App = () => {
     <AppContext.Provider value={{ state, dispatch }}>
       <div className="container">
         <SearchBooksForm />
+        <SearchResults />
       </div>
     </AppContext.Provider>
   );
