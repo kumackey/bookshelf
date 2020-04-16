956d94567e6d013f5e88cc7c61cd9c39766c831f
kumackey <kumak1t09e0@gmail.com>
2020-04-16 09:34:29 +0900

    display title and image

--- src/components/BooksShow.js
+++ src/components/BooksShow.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 import { getResponseFromId } from "../googleBooksAPI";
 
 const BooksShow = (props) => {
   const [item, setItem] = useState({});
+  const [title, setTitle] = useState("");
+  const [imageLink, setImageLink] = useState("");
   const id = props.match.params.id;
 
   const getItemFromId = async (id) => {
@@ -14,12 +17,25 @@ const BooksShow = (props) => {
 
   useEffect(() => {
     getItemFromId(id);
-  }, [id]);
+    if (item.volumeInfo) {
+      setTitle(item.volumeInfo.title);
+      setImageLink(item.volumeInfo.imageLinks.thumbnail);
+    }
+  }, [item.id]);
 
   return (
     <div className="container">
-      id:
-      {item.id}
+      <div className="row">
+        <div className="col-4">
+          <img src={imageLink} />
+        </div>
+        <div className="col-8">
+          <h4>title: {title}</h4>
+          <Link to="/">
+            <button className="btn btn-primary">戻る</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
